bb0ecce7ed7f52eae53fc8d39fb7ef79c2aeb7ed
zyonson <wakeup5216@gmail.com>
2023-08-14 23:02:34 +0900

    first commit

---
+++ src/main/java/com/zyonson/example/paidvacation/PaidvacationApplication.java
@@ -0,0 +1,13 @@
+package com.zyonson.example.paidvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaidvacationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaidvacationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyonson/example/paidvacation/PaidvacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyonson.example.paidvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaidvacationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
